52a54869d862957522138e59913b256eea3f3068
Alex_Hwang <3180102081@zju.edu.cn>
2020-06-02 14:39:02 +0800

    added database initialization procedure

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,14 @@
 from flask import Flask
 from flask import render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config.from_object('config')
+db = SQLAlchemy(app)
+
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
  return render_template('index.html')
+
+from app import models
