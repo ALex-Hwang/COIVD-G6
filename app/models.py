92b704abfec9318c74e3fe1eb52c9356d12ef762
Alex_Hwang <3180102081@zju.edu.cn>
2020-06-02 16:25:21 +0800

    modified the models.py

--- app/models.py
+++ app/models.py
@@ -1,24 +1,31 @@
 from app import db
 
-class User(db.Model):
-    __tablename__ = 'users'
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True)
-    email = db.Column(db.String(320), unique=True)
-    password = db.Column(db.String(32), nullable=False)
+class GoodsInfo(db.Model):
+    __tablename__  = 'GoodsInfo'
+    id = db.Column(db.String(10), primary_key=True, nullable=False, unique=True)
+    AdminID = db.Column(db.String(18), nullable=False, unique=True)
+    GoodsName = db.Column(db.String(30), nullable=False, unique=True)
+    GoodsSepc = db.Column(db.String(100), nullable=False, unique=True)
+    Stock = db.Column(db.Integer, nullable=False, unique=True)
+    OrderLimit = db.Column(db.Integer, nullable=False, unique=True)
+    DDL = db.Column(db.Date, nullable=False, unique=True)
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<Goods %r>' % self.GoodsName
 
-
-class Admin(db.Model):
-    __tablename__ = 'admins'
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True)
-    email = db.Column(db.String(320), unique=True)
-    password = db.Column(db.String(32), nullable=False)
+class OrderInfo(db.Model):
+    __tablename__ = 'OrderInfo'
+    id = db.Column(db.String(20), primary_key=True, nullable=False, unique=True)
+    IDNumber = db.Column(db.String(18), primary_key=False, nullable=False, unique=True) # The IDNumber of the applicant
+    GoodsID = db.Column(db.String(10), primary_key=False, nullable=False, unique=True, Foreign_key('GoodsInfo.id'))
+    OrderNum = db.Column(db.Integer, nullable=False, unique=True)
+    CreateTime = db.Column(db.Date, nullable=False, unique=True)
+    OrderToken = db.Column(db.String(8), nullable=False, unique=True)
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<Goods %r>' % self.id
+
+
+    
 
 
--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:948959254@localhost:3306/covid?charset=utf8'
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:948959254@localhost:3306/cvoid?charset=utf8'
 SQLALCHEMY_TRACK_MODIFICATIONS = True
